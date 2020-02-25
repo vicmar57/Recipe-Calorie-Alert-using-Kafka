e095ec8bd6f4e6b1fdb3ef535f19c08adcdb3869
victor martinov <vicmar57@gmail.com>
2020-02-25 22:20:13 +0200

    working files

---
+++ consumer-notification.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 17 18:00:10 2020
+
+@author: vicma
+"""
+
+import json
+from time import sleep
+
+from kafka import KafkaConsumer
+
+if __name__ == '__main__':
+    parsed_topic_name = 'parsed_recipes'
+    # Notify if a recipe has more than 200 calories
+    calories_threshold = 200
+
+    consumer = KafkaConsumer(parsed_topic_name, auto_offset_reset='earliest',
+                             bootstrap_servers=['localhost:9092'], api_version=(0, 10), consumer_timeout_ms=1000)
+    for msg in consumer:
+        record = json.loads(msg.value)
+        if record == {}:
+            print(msg)
+            continue
+        calories = int(record['calories'])
+        title = record['title']
+
+        if calories > calories_threshold:
+            print('Alert: {} calories count is {}'.format(title, calories))
+        sleep(3)
+
+    if consumer is not None:
+        consumer.close()
